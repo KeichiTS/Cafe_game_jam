bd07864f385cd4e4a92c62a53d40783f32a445a0
unknown <kkeichi@gmail.com>
2021-05-29 03:08:11 -0300

    Tileset, player, test objects and assets

---
+++ Player/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+var push_factor = 5
+var grow = 1
+
+func _process(delta):
+	scale = Vector2(grow,grow)
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+		
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector 
+		velocity = velocity * delta * 50 
+	else:
+		velocity = Vector2.ZERO  
+		
+	move_and_slide(velocity*100,Vector2(0,0),false, 10, PI, false)
+	
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		if collision.collider.is_in_group("bodies"):
+			collision.collider.apply_central_impulse(-collision.normal * velocity.length() * push_factor)
+	
